f70c85b38b898493700bc89785f09d468bdd1913
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2020-12-23 11:28:48 +0800

    added discord.utils.get()

--- attendance.py
+++ attendance.py
@@ -1,24 +1,27 @@
 # bot.py
 import os
-import time
+
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-    get_time()
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    
+    await client.close()
+
+
+client.run(TOKEN)
 
 
 def get_time():
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     print(current_time)
-
-client.run(TOKEN)
