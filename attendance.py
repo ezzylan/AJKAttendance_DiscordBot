79faea4603f60632451acfff891a3a37c3d81f5f
Ezlan Zulfiqree <66157842+ezzylan@users.noreply.github.com>
2021-01-06 14:19:06 +0800

    added everyone tag

--- attendance.py
+++ attendance.py
@@ -16,31 +16,31 @@ def att_link(day, hour):
     link = ""
     # lecture probstats
     if day == 1 and hour == 9:
-        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=335878\nAttendance Lecture ProbStats\nPassword minta kat Dehe"
+        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=335878\nAttendance Lecture ProbStats\nPassword minta kat Dehe\n@everyone"
     # lecture tcs
     elif day == 1 and hour == 15:
-        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=152862\nAttendance Lecture TCS"
+        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=152862\nAttendance Lecture TCS\n@everyone"
     # lecture softmod
     elif day == 2 and hour == 9:
-        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=143231\nAttendance Lecture SoftMod"
+        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=143231\nAttendance Lecture SoftMod\n@everyone"
     # lecture db
     elif day == 2 and hour == 13:
         link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=332871\nAttendance Lecture Database (Dr Khalit)"
     # lecture project management
     elif day == 3 and hour == 9:
-        link = "Attendance Lecture Project Management\nKetua group tutorial korang akan ambik attendance masing2. Jangan lupa hadir kelas ya!"
+        link = "Attendance Lecture Project Management\nKetua group tutorial korang akan ambik attendance masing2. Jangan lupa hadir kelas ya!\n@everyone"
     # tutorial probstats
     elif day == 3 and hour == 14:
         link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=337476\nAttendance Tutorial ProbStats\nPassword minta kat Dehe\n@everyone"
     # tutorial softmod
     elif day == 4 and hour == 9:
-        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=143231\nAttendance Tutorial SoftMod"
+        link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=143231\nAttendance Tutorial SoftMod\n@everyone"
     # tutorial db
     elif day == 4 and hour == 15:
         link = "https://spectrum.um.edu.my/mod/attendance/view.php?id=332912\nAttendance Tutorial Database (Dr Khalit)"
     # tutorial project management
     elif day == 5 and hour == 10:
-        link = "Attendance Tutorial Project Management\nKetua group tutorial korang akan ambik attendance masing2. Jangan lupa hadir kelas ya!"
+        link = "Attendance Tutorial Project Management\nKetua group tutorial korang akan ambik attendance masing2. Jangan lupa hadir kelas ya!\n@everyone"
     return link
 
 
@@ -57,7 +57,7 @@ async def on_ready():
                 hour = now.hour
                 link = att_link(day, hour)
                 try:
-                    message = await channel.send(link)
+                    await channel.send(link)
                 except:
                     pass
     await client.close()
